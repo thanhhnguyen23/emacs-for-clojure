fcb9421f1d82a6e1e7d151cd980fd4fe23932180
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2023-03-20 20:14:02 -0400

    add custom.el file

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(package-selected-packages
+   '(evil which-key treemacs-projectile treemacs-magit tagedit setup rainbow-delimiters lsp-ui lsp-treemacs lsp-ivy ivy-rich doom-themes doom-modeline counsel-projectile company clj-refactor cider-hydra all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
